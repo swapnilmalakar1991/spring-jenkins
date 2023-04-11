d4410ddf67bc405a0447a986471580634a711156
swapnilmalakar1991 <swapnilmalakar.1991@gmail.com>
2023-04-11 17:21:33 +0530

    First Commit

---
+++ src/test/java/com/globant/spring/boot/jenkins/SpringBootJenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.globant.spring.boot.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SpringBootJenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringBootJenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test Case Executing");
+		assertEquals(true, true);
+	}
+
+}
